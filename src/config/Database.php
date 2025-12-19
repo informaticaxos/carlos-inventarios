913745e24d545c60b6dd294a10943030cdae887b
informaticaxos <informaticaxos@gmail.com>
2025-12-18 19:31:40 -0500

    BUG: CORRECCION DE CONEXION

--- src/config/Database.php
+++ src/config/Database.php
@@ -14,7 +14,7 @@ class Database {
             $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
             $this->conn->exec("set names utf8");
         } catch(PDOException $exception) {
-            echo "Connection error: " . $exception->getMessage();
+            error_log("Connection error: " . $exception->getMessage());
         }
 
         return $this->conn;
