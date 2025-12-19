e6b16cfd50fc4097f22359fe2aaa02c4cde665ad
informaticaxos <informaticaxos@gmail.com>
2025-12-18 23:13:34 -0500

    a

--- src/config/Database.php
+++ src/config/Database.php
@@ -1,20 +1,17 @@
 <?php
 class Database {
-    private $host = "srv1575.hstgr.io";
-    private $db_name = "u564798502_cllaguarima";
-    private $username = "u564798502_cllaguarima";
-    private $password = "Cllaguarima.2025";
-    public $conn;
+    private static $conn = null;
 
-    public function getConnection() {
-        $this->conn = null;
-        try {
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $this->conn->exec("set names utf8");
-        } catch(PDOException $exception) {
-            echo "Error de conexión: " . $exception->getMessage();
+    public static function getConnection() {
+        if (self::$conn == null) {
+            try {
+                self::$conn = new PDO("mysql:host=" . "srv1575.hstgr.io" . ";dbname=" . "u564798502_cllaguarima", "u564798502_cllaguarima", "Cllaguarima.2025");
+                self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                self::$conn->exec("set names utf8");
+            } catch(PDOException $exception) {
+                echo "Error de conexión: " . $exception->getMessage();
+            }
         }
-        return $this->conn;
+        return self::$conn;
     }
 }
